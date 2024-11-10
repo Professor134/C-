0f69906f4311b862111fcedaa7c4da687dcb3a2a
Parth Chavan <parthchavan653@gmail.com>
2024-11-10 18:22:11 +0530

    Update

---
+++ linear queue insert delete.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+ int Q[100],MAX=5, ITEM, R=-1, F=1;
+  
+  int isfull()
+  {
+      if(R==MAX-1) 
+      {
+       return 1;
+      } 
+      else
+       return 0;
+  }
+  
+  int isempty()
+  {
+     if(F==-1)
+     {
+         return 1;
+     }
+     else
+      return 0;
+  }
+  
+  int insert()
+  {
+     if(isfull())
+     {
+        printf("stack is full");
+     }
+     else
+     {
+        printf("\n enter no to insert=");
+        scanf("%d",&ITEM);
+        if(R==-1)
+           F=R=0;
+        else
+          R=R+1;
+          printf("\n %d inserted",ITEM);
+        Q[R]=ITEM;
+     }
+  }
+  
+  int delete()
+  {
+      if(isempty())
+         printf("stack is empty");
+      else
+      {
+         ITEM=Q[F];
+         if(F==R)
+           F=R=-1;
+         else
+           F=F+1;
+         printf("\n %d deleted",ITEM);    
+      }   
+  }
+  
+  int main()
+  {
+  insert();
+  insert();
+  insert();
+  insert();
+  delete();
+  delete();
+  
+  }
